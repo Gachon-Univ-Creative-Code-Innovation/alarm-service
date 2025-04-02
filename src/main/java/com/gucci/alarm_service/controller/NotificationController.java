9544a608410d0f9b532eaa662146b772fc30ed3a
jeonjunbae <wnsqowid0901@gmail.com>
2025-04-02 22:25:37 +0900

    feat: SSE 구독 기능 추가
    - `/api/notifications/subscribe/{userId]` 엔드포인트로 GET 요청 시 SSE 연결
    
    JIRA: GUC-41
    resolves: #3

--- src/main/java/com/gucci/alarm_service/controller/NotificationController.java
+++ src/main/java/com/gucci/alarm_service/controller/NotificationController.java
@@ -1,21 +1,23 @@
 package com.gucci.alarm_service.controller;
 
-import com.gucci.alarm_service.domain.Notification;
 import com.gucci.alarm_service.dto.NotificationRequest;
 import com.gucci.alarm_service.dto.NotificationResponse;
 import com.gucci.alarm_service.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/notifications")
 public class NotificationController {
     private final NotificationService notificationService;
+
+    @GetMapping( "/subscribe/{userId}")
+    public SseEmitter subscribe(@PathVariable Long userId) {
+        return notificationService.subscribe(userId);
+    }
 
     @PostMapping("/send")
     public ResponseEntity<NotificationResponse> alarmCreate(@RequestBody NotificationRequest notificationRequest) {
--- src/main/java/com/gucci/alarm_service/service/NotificationService.java
+++ src/main/java/com/gucci/alarm_service/service/NotificationService.java
@@ -1,16 +1,19 @@
 package com.gucci.alarm_service.service;
 
+import com.gucci.alarm_service.common.SseEmitterManager;
 import com.gucci.alarm_service.domain.Notification;
 import com.gucci.alarm_service.dto.NotificationRequest;
 import com.gucci.alarm_service.dto.NotificationResponse;
 import com.gucci.alarm_service.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
     private final NotificationRepository notificationRepository;
+    private final SseEmitterManager sseEmitterManager;
 
     public NotificationResponse save(NotificationRequest request){
         Notification notification = Notification.builder()
@@ -25,5 +28,9 @@ public class NotificationService {
         Notification save = notificationRepository.save(notification);
 
         return NotificationResponse.from(save);
+    }
+
+    public SseEmitter subscribe(Long userId) {
+        return sseEmitterManager.connect(userId);
     }
 }
