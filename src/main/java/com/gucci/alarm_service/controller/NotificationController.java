91868138541535ead3779cde9cf4d7c8d2825d77
jeonjunbae <wnsqowid0901@gmail.com>
2025-04-12 21:19:34 +0900

    style: 테스트용 API 주석 추가
    JIRA: GUC-73
    resolves: #11

--- src/main/java/com/gucci/alarm_service/controller/NotificationController.java
+++ src/main/java/com/gucci/alarm_service/controller/NotificationController.java
@@ -21,17 +21,24 @@ public class NotificationController {
     private final NotificationReadService notificationReadService;
     private final NotificationEventHandler notificationEventHandler;
 
-    // 알림 연결(구독)
+    // 알림 연결(구독) / 테스트용 (SSE 로직에 구현함)
     @GetMapping("/subscribe/{userId}")
     public SseEmitter subscribe(@PathVariable Long userId) {
         return notificationEventHandler.subscribe(userId);
     }
 
-    // 알림 전송
+    // 알림 전송 / 테스트용 (SSE 로직에 구현함)
     @PostMapping("/send")
     public ApiResponse<NotificationResponse> alarmCreate(@RequestBody NotificationRequest notificationRequest) {
         NotificationResponse save = notificationWriteService.save(notificationRequest);
         return ApiResponse.success(save);
+    }
+
+    // 읽지 않은 알림 여부 / 테스트용 (SSE 로직에 구현함)
+    @GetMapping("/unread/exists")
+    public ApiResponse<Boolean> unreadAlarmExist(@RequestHeader("X-USER-ID") Long receiverId) {
+        boolean hasUnread = notificationReadService.existUnreadAlarm(receiverId);
+        return ApiResponse.success(hasUnread);
     }
 
     // 전체 알림 조회
@@ -74,13 +81,6 @@ public class NotificationController {
         notificationWriteService.deleteAll(receiverId);
 
         return ApiResponse.success();
-    }
-
-    // 읽지 않은 알림 여부
-    @GetMapping("/unread/exists")
-    public ApiResponse<Boolean> unreadAlarmExist(@RequestHeader("X-USER-ID") Long receiverId) {
-        boolean hasUnread = notificationReadService.existUnreadAlarm(receiverId);
-        return ApiResponse.success(hasUnread);
     }
 
 }
